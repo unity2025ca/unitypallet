02fedec133fbee66642c0eaa2ec491d8d8428f29
kamaljaberjabe <41778121-kamaljaberjabe@users.noreply.replit.com>
2025-05-20 23:22:45 +0000

    Restored to '96d23afa4cd82d2f31ebc155d7a8920d9c9136f0'
    Replit-Restored-To: 96d23afa4cd82d2f31ebc155d7a8920d9c9136f0

--- server/index.ts
+++ server/index.ts
@@ -85,11 +85,12 @@ app.use((req, res, next) => {
     serveStatic(app);
   }
 
-  // En producción usa el puerto definido en las variables de entorno
-  // o el puerto asignado por cPanel, en desarrollo usa el puerto 5000
-  const port = process.env.PORT || 5000;
+  // ALWAYS serve the app on port 5000
+  // this serves both the API and the client.
+  // It is the only port that is not firewalled.
+  const port = 5000;
   server.listen({
-    port: Number(port),
+    port,
     host: "0.0.0.0",
     reusePort: true,
   }, () => {
